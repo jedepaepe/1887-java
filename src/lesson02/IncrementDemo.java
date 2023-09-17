90c24c48a6e34d4022bf064c82d8d26ae57ee1ac
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-17 12:49:00 +0200

    demo increment

---
+++ src/lesson02/IncrementDemo.java
@@ -0,0 +1,20 @@
+package lesson02;
+
+public class IncrementDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+        a++;
+        System.out.println(a);
+
+        ++a;
+        System.out.println(a);
+
+        --a;
+        System.out.println(a);
+
+        a--;
+        System.out.println(a);
+    }
+}
---
+++ src/lesson02/IncrementDemoSimplified.java
@@ -0,0 +1,18 @@
+package lesson02;
+
+public class IncrementDemoSimplified {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+        System.out.println(a++);
+
+        System.out.println(++a);
+
+        System.out.println(--a);
+
+        System.out.println(a--);
+
+        System.out.println(a);
+    }
+}
