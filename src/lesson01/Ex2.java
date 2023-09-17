34d7cee21a06df2fe980a700dcb84beae250aad0
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-17 09:48:12 +0200

    ex 2

---
+++ src/lesson01/Ex2.java
@@ -0,0 +1,11 @@
+package lesson01;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        System.out.println("ex2 fait");
+        System.out.println("ex2 fait");
+        System.out.println("ex2 fait");
+        System.out.println("ex2 fait");
+        System.out.println("ex2 fait");
+    }
+}
