f8482a8f367848d2be635f765efae27cb86d9edc
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-17 12:02:10 +0200

    exercice 5

---
+++ src/lesson02/Ex5.java
@@ -0,0 +1,12 @@
+package lesson02;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        System.out.println(2 * (5 / 2 + 5 / 2));
+        System.out.println(2 * 5 / 2 + 2 * 5 / 2);
+        System.out.println(2 * (5 / 2));
+        System.out.println(2 * 5 / 2);
+        System.out.println(2 * 5 / 2 + 5 / 2);
+        System.out.println(3 + 4 * 4 + 5 * (4 + 1) - 1);
+    }
+}
