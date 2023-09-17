1420b07435449360406f8aa08276dc7ec7e98ba9
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-17 12:29:01 +0200

    exercice 6

---
+++ src/lesson02/Ex6.java
@@ -0,0 +1,10 @@
+package lesson02;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        System.out.println(0b10);
+        System.out.println(010);
+        System.out.println(10);
+        System.out.println(0x10);
+    }
+}
