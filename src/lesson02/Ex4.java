4ae103f7de527721a793e9c231b9453316244d53
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-17 12:11:51 +0200

    exercice 5

---
+++ src/lesson02/Ex4.java
@@ -0,0 +1,11 @@
+package lesson02;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        System.out.println(56 % 6);
+        System.out.println(78 % -4);
+        System.out.println(-34 % 5);
+        System.out.println(5 % 1);
+        System.out.println(1 % 5);
+    }
+}
---
+++ src/lesson02/Ex8Limits.java
@@ -0,0 +1,24 @@
+package lesson02;
+
+public class Ex8Limits {
+    public static void main(String[] args) {
+        System.out.println("bytes");
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println();
+
+        System.out.println("short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println();
+
+        System.out.println("int");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println();
+
+        System.out.println("long");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+    }
+}
