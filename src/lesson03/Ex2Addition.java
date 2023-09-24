657590de2a1c961a3b2e4a6db094d059b3fe9568
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-24 11:11:13 +0200

    exercice

---
+++ src/lesson03/Ex2Addition.java
@@ -0,0 +1,12 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Ex2Addition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        System.out.println(value1 + value2);
+    }
+}
