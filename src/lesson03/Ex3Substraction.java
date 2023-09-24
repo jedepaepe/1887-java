11c5f14cad2dc5e4cb28e3b13d38514adc2e6841
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-24 11:22:28 +0200

    exercice

---
+++ src/lesson03/Ex3Substraction.java
@@ -0,0 +1,12 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Ex3Substraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        System.out.println(value1 - value2);
+    }
+}
