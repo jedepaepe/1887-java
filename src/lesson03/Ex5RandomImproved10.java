b24d8c8b45d402b2995fa8d573d0b2e66b4e7029
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-24 11:52:08 +0200

    exercice

--- src/lesson03/Ex5RandomImproved10.java
+++ src/lesson03/Ex5RandomImproved10.java
@@ -7,15 +7,15 @@ public class Ex5RandomImproved10 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Veuillez introduire la valeur maximum du nombre aléatoire: ");
         int max = scanner.nextInt();
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
-        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + Math.floor(max * Math.random()));
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
+        System.out.println("nombre aléatoire: " + max * Math.random());
     }
 }
---
+++ src/lesson03/Ex5RandomImproved10Int.java
@@ -0,0 +1,21 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Ex5RandomImproved10Int {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez introduire la valeur maximum du nombre aléatoire: ");
+        int max = scanner.nextInt();
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+        System.out.println("nombre aléatoire: " + (int) (max * Math.random()));
+    }
+}
