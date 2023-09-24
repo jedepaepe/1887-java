d59116b01a980900cb13404f799591f588d9c6e4
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-24 10:59:39 +0200

    exercice

---
+++ src/lesson03/Ex1MultiplyBy2.java
@@ -0,0 +1,11 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class Ex1MultiplyBy2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println(2 * value);
+    }
+}
