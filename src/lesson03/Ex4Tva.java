56dab4c7817af931ca14139e950260d7a9f10a0f
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-26 16:14:13 +0200

    exercice

--- src/lesson03/Ex4Tva.java
+++ src/lesson03/Ex4Tva.java
@@ -2,12 +2,12 @@ package lesson03;
 
 import java.util.Scanner;
 
-public class Ex3Tva {
+public class Ex4Tva {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("prix et taux de tva: ");
         double price = scanner.nextDouble();
         double vat = scanner.nextDouble();
-        System.out.println("prix tva incluse: " + (price * vat / 100));
+        System.out.println("prix tva incluse: " + (price * (1 + vat / 100)));
     }
 }
