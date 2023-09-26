65d31888fa967e5d960011529ae96cfa6ee833d1
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-26 16:38:15 +0200

    exercice

---
+++ src/lesson03/CylinderVolumeCalculator.java
@@ -0,0 +1,17 @@
+package lesson03;
+
+import java.util.Scanner;
+
+public class CylinderVolumeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("hauteur: ");
+        double height = scanner.nextDouble();
+        System.out.print("rayon: ");
+        double radius = scanner.nextDouble();
+        double area = radius * radius * Math.PI;
+        double volume = height * area;
+        System.out.println("aire: " + area);
+        System.out.println("volume: " + volume);
+    }
+}
