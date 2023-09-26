cbdd78abd36820da4a8d506759d26b7cdf642c8c
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-09-26 18:17:34 +0200

    exercice

--- src/lesson04/Ex3FindARandom.java
+++ src/lesson04/Ex3FindARandom.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Ex3FindARandom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduire un entier entre 0 et 4");
+        System.out.print("Introduire un entier entre 0 et 4: ");
         int guess = scanner.nextInt();
         int random = (int) (5 * Math.random());
         System.out.println("gagné : " + (guess == random));
+        System.out.println("le nombre était: " + random);
     }
 }
