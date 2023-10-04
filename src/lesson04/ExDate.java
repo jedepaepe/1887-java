944d86a0ab7653b89a2beb290c6a757e9d2af15a
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-04 22:54:15 +0200

    localdate

--- src/lesson04/ExDate.java
+++ src/lesson04/ExDate.java
@@ -12,7 +12,7 @@ public class ExDate {
         int day = scanner.nextInt();
         LocalDate now = LocalDate.now();
         System.out.println("l'année est " + now.getYear() + ", votre réponse est " + (now.getYear() == year));
-        System.out.println("le mois est " + now.getMonth() + ", votre réponse est " + (now.getMonthValue() == month));
+        System.out.println("le mois est " + now.getMonthValue() + ", votre réponse est " + (now.getMonthValue() == month));
         System.out.println("le jour est " + now.getDayOfMonth() + ", votre réponse est " + (now.getDayOfMonth() == day));
     }
 }
