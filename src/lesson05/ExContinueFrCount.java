e8d1270bae34bafc55848c03bdc263e3acb5a358
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-08 09:26:00 +0200

    exercice while

---
+++ src/lesson05/ExContinueFrCount.java
@@ -0,0 +1,17 @@
+package lesson05;
+
+import java.util.Scanner;
+
+public class ExContinueFrCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String next = "oui";
+        while (next.equals("oui")) {
+            count = count + 1;
+            System.out.print("Voulez-vous continuer? ");
+            next = scanner.next();
+        }
+        System.out.println("Le nombre d'itération est " + count);
+    }
+}
