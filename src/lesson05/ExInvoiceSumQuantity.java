645b9cae989c902104e196ddd8c94634741ada40
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-08 09:48:31 +0200

    exercice while

---
+++ src/lesson05/ExInvoiceSumQuantity.java
@@ -0,0 +1,20 @@
+package lesson05;
+
+import java.util.Scanner;
+
+public class ExInvoiceSumQuantity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double total = 0;
+        String next = "oui";
+        while (next.equals("oui")) {
+            System.out.print("Prix de l'article et la quantité: ");
+            double price = scanner.nextDouble();
+            double quantity = scanner.nextDouble();
+            total = total + price * quantity;
+            System.out.print("Nouvelle ligne de facture? ");
+            next = scanner.next();
+        }
+        System.out.println("Le total de la facture est: " + total);
+    }
+}
