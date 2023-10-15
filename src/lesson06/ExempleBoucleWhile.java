e166c703ca1efcd51484595af25d524573f01ac0
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-15 10:08:07 +0200

    exercice while

---
+++ src/lesson06/ExempleBoucleWhile.java
@@ -0,0 +1,11 @@
+package lesson06;
+
+public class ExempleBoucleWhile {
+    public static void main(String[] args) {
+        int lineNr = 1;
+        while (lineNr <= 10) {
+            System.out.println(lineNr);
+            lineNr++;
+        }
+    }
+}
