efce41a2c58aa214542ebaf096ef72367e13925e
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-15 17:48:26 +0200

    if ex

---
+++ src/lesson06/ExempleIf.java
@@ -0,0 +1,13 @@
+package lesson06;
+
+public class ExempleIf {
+    public static void main(String[] args) {
+        int nombre = 10;
+
+        if (nombre % 2 == 0) {
+            System.out.println(nombre + " est un nombre pair.");
+        } else {
+            System.out.println(nombre + " est un nombre impair.");
+        }
+    }
+}
