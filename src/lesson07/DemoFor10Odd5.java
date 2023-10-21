9098cbe2c90899ab244b46145433580625fa3025
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-21 15:57:03 +0200

    for

---
+++ src/lesson07/DemoFor10Odd5.java
@@ -0,0 +1,9 @@
+package lesson07;
+
+public class DemoFor10Odd5 {
+    public static void main(String[] args) {
+        for (int i = -5; i <= 17; i = i + 2) {
+            System.out.println("itération " + i);
+        }
+    }
+}
