7d54c2ce0eee888c99f96a12743fa4590cb7a647
Jean-Michel DEPAEPE <AJA063@equans.com>
2023-10-21 16:02:16 +0200

    for

---
+++ src/lesson07/ExMultiplicationTable.java
@@ -0,0 +1,12 @@
+package lesson07;
+
+public class ExMultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; ++i) {
+            for (int j = 1; j <= 10; ++j) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
